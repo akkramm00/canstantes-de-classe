263ae2e181469ce5f3b933d5dd67885019ad323e
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-07-22 21:51:12 +0000

    commit first constante

--- index.php
+++ index.php
@@ -5,12 +5,15 @@
   <body>
     <?php 
 class Facture{
+  const TVA = 20 ; //instancier une constante de classe 
   public function montantTTC($ht) {
   return $ht*(1+20/100);
   }
 }
 $facture1 = new Facture();
 echo $facture1 ->montantTTC(1000);
+echo "<br>";
+echo Facture::TVA ;// "::" s'appelle l'operateur de resolution de porté.
 ?> 
 
   <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
